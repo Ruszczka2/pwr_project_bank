85a61266331124e87069f44ae158edd939cb7ab2
Natalia <nataliaoa2002@gmail.com>
2026-05-01 19:21:34 +0200

    feat: uzupełnienie reszty pól wejściowych

--- app.R
+++ app.R
@@ -6,7 +6,16 @@ ui <- fluidPage(
     tabPanel("Kalkulator", 
              fluidRow(
               column(6, 
-              numericInput(inputId = "age", label = "Wiek", value = 30)),
+              numericInput(inputId = "age", label = "Wiek", value = 30),
+              numericInput(inputId = "MonthlyIncome", label = "Miesięczny dochów", value = 5000),
+              numericInput(inputId = "DebtRatio", label = "Wskaźnik zadłużenia", value = 0.3),
+              numericInput(inputId = "NumberOfDependents", label = "Liczba osób na utrzymaniu", value = 0),
+              numericInput(inputId = "RevolvingUtilizationOfUnsecuredLines", label = "Wykorzystanie limitu kredytowego", value = 0.5),
+              numericInput(inputId = "NumberOfOpenCreditLinesAndLoans", label = "Liczba otwartych linii kredytowyc", value = 5),
+              numericInput(inputId = "NumberRealEstateLoansOrLines", label = "Liczba kredytów hipotecznych", value = 0),
+              numericInput(inputId = "NumberOfTime30-59DaysPastDueNotWorse", label = "Opóźnienia 30-59 dni", value = 0),
+              numericInput(inputId = "NumberOfTime60-89DaysPastDueNotWorse", label = "Opóźnienia 60-89 dni", value = 0),
+              numericInput(inputId = "NumberOfTimes90DaysLate", label = "Opóźnienia 90+ dni", value = 0)),
               column(6, )
              )
     ))
