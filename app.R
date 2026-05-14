1d14a100e2222e0e86b622be7e3ff74e9c6aa3cc
Natalia <nataliaoa2002@gmail.com>
2026-05-14 09:04:33 +0200

    feat: walidacja zakresów inputów w kalkulatorze

--- app.R
+++ app.R
@@ -15,16 +15,16 @@ ui <- fluidPage(
     tabPanel("Kalkulator", 
              fluidRow(
                column(6, 
-                      numericInput(inputId = "age", label = "Wiek", value = 30),
-                      numericInput(inputId = "MonthlyIncome", label = "Miesięczny dochód brutto [PLN]", value = 5000),
-                      numericInput(inputId = "DebtRatio", label = "Wskaźnik zadłużenia", value = 0.3),
-                      numericInput(inputId = "NumberOfDependents", label = "Liczba osób na utrzymaniu", value = 0),
-                      numericInput(inputId = "RevolvingUtilizationOfUnsecuredLines", label = "Wykorzystanie limitu kredytowego", value = 0.5),
-                      numericInput(inputId = "NumberOfOpenCreditLinesAndLoans", label = "Liczba otwartych linii kredytowych", value = 5),
-                      numericInput(inputId = "NumberRealEstateLoansOrLines", label = "Liczba kredytów hipotecznych", value = 0),
-                      numericInput(inputId = "NumberOfTime30-59DaysPastDueNotWorse", label = "Opóźnienia 30-59 dni w ostatnich 2 latach", value = 0),
-                      numericInput(inputId = "NumberOfTime60-89DaysPastDueNotWorse", label = "Opóźnienia 60-89 dni w ostatnich 2 latach", value = 0),
-                      numericInput(inputId = "NumberOfTimes90DaysLate", label = "Opóźnienia 90+ dni w ostatnich 2 latach", value = 0),
+                      numericInput(inputId = "age", label = "Wiek", value = 30, min = 0, max = 109),
+                      numericInput(inputId = "MonthlyIncome", label = "Miesięczny dochód brutto [PLN]", value = 5000, min = 0, max = 3008750),
+                      numericInput(inputId = "DebtRatio", label = "Wskaźnik zadłużenia", value = 0.3, min = 0, max = 329664),
+                      numericInput(inputId = "NumberOfDependents", label = "Liczba osób na utrzymaniu", value = 0, min = 0, max = 20),
+                      numericInput(inputId = "RevolvingUtilizationOfUnsecuredLines", label = "Wykorzystanie limitu kredytowego", value = 0.5, min = 0, max = 50708),
+                      numericInput(inputId = "NumberOfOpenCreditLinesAndLoans", label = "Liczba otwartych linii kredytowych", value = 5, min = 0, max = 58),
+                      numericInput(inputId = "NumberRealEstateLoansOrLines", label = "Liczba kredytów hipotecznych", value = 0, min = 0, max = 54),
+                      numericInput(inputId = "NumberOfTime30.59DaysPastDueNotWorse", label = "Opóźnienia 30-59 dni w ostatnich 2 latach", value = 0, min = 0, max = 98),
+                      numericInput(inputId = "NumberOfTime60.89DaysPastDueNotWorse", label = "Opóźnienia 60-89 dni w ostatnich 2 latach", value = 0, min = 0, max = 98),
+                      numericInput(inputId = "NumberOfTimes90DaysLate", label = "Opóźnienia 90+ dni w ostatnich 2 latach", value = 0, min = 0, max = 98),
                       actionButton(inputId = "predic_button", label = "Oblicz ryzyko")),
                column(6, textOutput(outputId = "wynik"))
              )
@@ -49,16 +49,47 @@ server <- function(input, output) {
   
   wynik_predykcji <- eventReactive(input$predic_button, {
     
+    if (input$age < 0 || input$age > 109) {
+      return("Nieprawidłowa wartość w kolumnie 'Wiek'. Prawidłowy zakres: 0-109.")
+    }
+    if (input$MonthlyIncome < 0 || input$MonthlyIncome > 3008750) {
+      return("Nieprawidłowa wartość w kolumnie 'Miesięczny dochód'. Prawidłowy zakres: 0-3008750.")
+    }
+    if (input$DebtRatio < 0 || input$DebtRatio > 329664) {
+      return("Nieprawidłowa wartość w kolumnie 'Wskaźnik zadłużenia'. Prawidłowy zakres: 0-329664.")
+    }
+    if (input$NumberOfDependents < 0 || input$NumberOfDependents > 20) {
+      return("Nieprawidłowa wartość w kolumnie 'Liczba osób na utrzymaniu'. Prawidłowy zakres: 0-20.")
+    }
+    if (input$RevolvingUtilizationOfUnsecuredLines < 0 || input$RevolvingUtilizationOfUnsecuredLines > 50708) {
+      return("Nieprawidłowa wartość w kolumnie 'Wykorzystanie limitu kredytowego'. Prawidłowy zakres: 0-50708.")
+    }
+    if (input$NumberOfOpenCreditLinesAndLoans < 0 || input$NumberOfOpenCreditLinesAndLoans > 58) {
+      return("Nieprawidłowa wartość w kolumnie 'Liczba otwartych linii kredytowych'. Prawidłowy zakres: 0-58.")
+    }
+    if (input$NumberRealEstateLoansOrLines < 0 || input$NumberRealEstateLoansOrLines > 54) {
+      return("Nieprawidłowa wartość w kolumnie 'Liczba kredytów hipotecznych'. Prawidłowy zakres: 0-54.")
+    }
+    if (input$NumberOfTime30.59DaysPastDueNotWorse < 0 || input$NumberOfTime30.59DaysPastDueNotWorse > 98) {
+      return("Nieprawidłowa wartość w kolumnie 'Opóźnienia 30-59 dni'. Prawidłowy zakres: 0-98.")
+    }
+    if (input$NumberOfTime60.89DaysPastDueNotWorse < 0 || input$NumberOfTime60.89DaysPastDueNotWorse > 98) {
+      return("Nieprawidłowa wartość w kolumnie 'Opóźnienia 60-89 dni'. Prawidłowy zakres: 0-98.")
+    }
+    if (input$NumberOfTimes90DaysLate < 0 || input$NumberOfTimes90DaysLate > 98) {
+      return("Nieprawidłowa wartość w kolumnie 'Opóźnienia 90+ dni'. Prawidłowy zakres: 0-98.")
+    }
+    
     nowe_dane <- data.frame(
       RevolvingUtilizationOfUnsecuredLines = input$RevolvingUtilizationOfUnsecuredLines,
       age = input$age,
-      NumberOfTime30.59DaysPastDueNotWorse = input[["NumberOfTime30-59DaysPastDueNotWorse"]],
+      NumberOfTime30.59DaysPastDueNotWorse = input[["NumberOfTime30.59DaysPastDueNotWorse"]],
       DebtRatio = input$DebtRatio,
       MonthlyIncome = input$MonthlyIncome,
       NumberOfOpenCreditLinesAndLoans = input$NumberOfOpenCreditLinesAndLoans,
       NumberOfTimes90DaysLate = input$NumberOfTimes90DaysLate,
       NumberRealEstateLoansOrLines = input$NumberRealEstateLoansOrLines,
-      NumberOfTime60.89DaysPastDueNotWorse = input[["NumberOfTime60-89DaysPastDueNotWorse"]],
+      NumberOfTime60.89DaysPastDueNotWorse = input[["NumberOfTime60.89DaysPastDueNotWorse"]],
       NumberOfDependents = input$NumberOfDependents
     )
     
